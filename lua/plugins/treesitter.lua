cbd125d71159e98e8efe34809035a6ef5032151b
Ashutosh Mishra <ashutosh.mishra@araymond.com>
2025-07-09 22:09:40 +0200

    chore: added tressitter textobjects, context and refactor

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,226 @@
-return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
+local treesitter_parsers = {
+  'bash',
+  'c',
+  'cpp',
+  'gitcommit',
+  'html',
+  'json',
+  'just',
+  'lua',
+  'markdown',
+  'markdown_inline', -- markdown code blocks
+  'meson',
+  'python',
+  'rust',
+  'vim',
+  'vimdoc',
+  'yaml',
+}
+
+-- Text objects configuration
+local textobjects = {
+  select = {
+    enable = true,
+    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+    keymaps = {
+      -- You can use the capture groups defined in textobjects.scm
+      ['aa'] = '@parameter.outer',
+      ['ia'] = '@parameter.inner',
+      ['af'] = '@function.outer',
+      ['if'] = '@function.inner',
+      ['ac'] = '@class.outer',
+      ['ic'] = '@class.inner',
+      ['al'] = '@loop.outer',
+      ['il'] = '@loop.inner',
+      ['ab'] = '@block.outer',
+      ['ib'] = '@block.inner',
+      ['as'] = '@statement.outer',
+      ['is'] = '@statement.inner',
+      ['ad'] = '@conditional.outer',
+      ['id'] = '@conditional.inner',
+      ['am'] = '@call.outer',
+      ['im'] = '@call.inner',
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    -- You can choose the select mode (default is charwise 'v')
+    selection_modes = {
+      ['@parameter.outer'] = 'v', -- charwise
+      ['@function.outer'] = 'V', -- linewise
+      ['@class.outer'] = '<c-v>', -- blockwise
+    },
+    -- If you set this to `true` (default is `false`) then any textobject is
+    -- extended to include preceding or succeeding whitespace. Succeeding
+    -- whitespace has priority in order to act similar to eg the built-in
+    -- `ap` and `ip` mappings.
+    include_surrounding_whitespace = true,
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      [']f'] = '@function.outer',
+      [']c'] = '@class.outer',
+      [']a'] = '@parameter.inner',
+      [']m'] = '@call.outer',
+    },
+    goto_next_end = {
+      [']F'] = '@function.outer',
+      [']C'] = '@class.outer',
+      [']A'] = '@parameter.inner',
+      [']M'] = '@call.outer',
+    },
+    goto_previous_start = {
+      ['[f'] = '@function.outer',
+      ['[c'] = '@class.outer',
+      ['[a'] = '@parameter.inner',
+      ['[m'] = '@call.outer',
+    },
+    goto_previous_end = {
+      ['[F'] = '@function.outer',
+      ['[C'] = '@class.outer',
+      ['[A'] = '@parameter.inner',
+      ['[M'] = '@call.outer',
+    },
+  },
+  swap = {
+    enable = true,
+    swap_next = {
+      ['<leader>a'] = '@parameter.inner',
+      ['<leader>f'] = '@function.outer',
+      ['<leader>m'] = '@call.outer',
+    },
+    swap_previous = {
+      ['<leader>A'] = '@parameter.inner',
+      ['<leader>F'] = '@function.outer',
+      ['<leader>M'] = '@call.outer',
+    },
+  },
+  lsp_interop = {
+    enable = true,
+    border = 'none',
+    floating_preview_opts = {},
+    peek_definition_code = {
+      ['<leader>df'] = '@function.outer',
+      ['<leader>dF'] = '@class.outer',
+    },
+  },
+}
+
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-refactor',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/nvim-treesitter-context',
+      'RRethy/nvim-treesitter-textsubjects',
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = treesitter_parsers,
+        sync_install = false,
+        ignore_install = {},
+        auto_install = true,
+
+        -- Core highlighting
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+
+        -- Code indentation
+        indent = { enable = true },
+
+        -- Incremental selection
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<CR>',
+            node_incremental = '<CR>',
+            node_decremental = '<BS>',
+            scope_incremental = '<TAB>',
+          },
+        },
+
+        -- Text objects (from nvim-treesitter-textobjects)
+        textobjects = textobjects,
+
+        -- Text subjects (from nvim-treesitter-textsubjects)
+        textsubjects = {
+          enable = true,
+          prev_selection = ',',
+          keymaps = {
+            ['.'] = { 'textsubjects-smart', desc = 'Select the current text subject' },
+            ['a;'] = {
+              'textsubjects-container-outer',
+              desc = 'Select outer container (class, function, etc.)',
+            },
+            ['i;'] = {
+              'textsubjects-container-inner',
+              desc = 'Select inside containers (classes, functions, etc.)',
+            },
+          },
+        },
+
+        -- Refactor module (from nvim-treesitter-refactor)
+        refactor = {
+          highlight_definitions = {
+            enable = true,
+            clear_on_cursor_move = true,
+          },
+          highlight_current_scope = { enable = false },
+          smart_rename = {
+            enable = true,
+            keymaps = {
+              smart_rename = '<leader>rr',
+            },
+          },
+          navigation = {
+            enable = true,
+            keymaps = {
+              goto_definition = '<leader>rd',
+              list_definitions = '<leader>rl',
+              list_definitions_toc = '<leader>rh',
+              goto_next_usage = '<leader>rj',
+              goto_previous_usage = '<leader>rk',
+            },
+          },
+        },
+
+        -- Matchup integration
+        matchup = { enable = true },
+
+        -- Context commentstring
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      }
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      enable = true,
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    },
+  },
 }
